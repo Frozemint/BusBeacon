89a107130ac999ffda0f4508b220139a085bf319
Nami <nami5504@gmail.com>
2018-11-25 17:23:42 -0800

    Initial Commit

---
+++ BusBeacon2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusBeacon2
+//
+//  Created by Nami on 2018-11-25.
+//  Copyright © 2018 Nami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
